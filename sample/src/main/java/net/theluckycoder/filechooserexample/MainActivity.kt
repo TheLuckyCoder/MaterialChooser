8f7bd9bf030cbc18835316a564867599a2eed0c0
Razvan <razvan.filea@gmail.com>
2019-06-12 17:48:31 +0300

    Switch to AndroidX

--- sample/src/main/java/net/theluckycoder/filechooserexample/MainActivity.kt
+++ sample/src/main/java/net/theluckycoder/filechooserexample/MainActivity.kt
@@ -2,7 +2,7 @@ package net.theluckycoder.filechooserexample
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.EditText
 import android.widget.Switch
